72ad9471774809bda0e770aa6f83778ec34b8d04
Sammy Harris <sammy@stegabob.com>
2023-09-13 14:03:05 -0500

    remove linux from cert script

--- generate-cert.sh
+++ generate-cert.sh
@@ -8,11 +8,12 @@ if [ "$(uname)" == "Darwin" ]; then
   echo "Adding \`Sunspot\` Certificate Authority to your trusted login certificates..."
   security add-trusted-cert -p ssl -k ~/Library/Keychains/login.keychain-db ./certs/sunspot.pem
   echo "Done!"
-elif [ "$(uname)" == "Linux" ] && [ -d "/usr/local/share/ca-certificates/" ]; then
-  echo "Copying \`Sunspot\` Certificate Authority to \`/usr/local/share/ca-certificates/sunspot.crt\` and updating trusted certificates..."
-  sudo cp ./certs/sunspot.pem /usr/local/share/ca-certificates/sunspot.crt
-  sudo update-ca-certificates
-  echo "Done!"
 else
-  echo "Please add \`./certs/sunspot.pem\` to your trusted certificates."
+#elif [ "$(uname)" == "Linux" ] && [ -d "/usr/local/share/ca-certificates/" ]; then
+#  echo "Copying \`Sunspot\` Certificate Authority to \`/usr/local/share/ca-certificates/sunspot.crt\` and updating trusted certificates..."
+##  sudo cp ./certs/sunspot.pem /usr/local/share/ca-certificates/sunspot.crt
+##  sudo update-ca-certificates
+#  echo "Done!"
+#else
+  echo "Please add \`./certs/sunspot.pem\` to your browser. See the README instructions."
 fi
