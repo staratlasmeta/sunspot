0ff1e4c3cdb9e6c99dddf3fc421efb9dfa7887fc
Sammy Harris <sammy@stegabob.com>
2023-09-12 19:10:22 -0500

    update script

--- generate-cert.sh
+++ generate-cert.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "Generating \`Sunspot\` Certificate Authority Key and Certificate..."
 echo ""
-mkdir -p ./certificates
-openssl genrsa -out src/ca/sunspot.key 2048
-openssl req -x509 -new -nodes -key ./certificates/sunspot.key -sha256 -days 1825 -out ./certificates/sunspot.pem
+mkdir -p ./certs
+openssl genrsa -out ./certs/sunspot.key 2048
+openssl req -x509 -new -nodes -key ./certs/sunspot.key -sha256 -days 1825 -out ./certs/sunspot.pem
 if [ "$(uname)" == "Darwin" ]; then
   echo "Adding \`Sunspot\` Certificate Authority to your trusted login certificates..."
-  security add-trusted-cert -p ssl -k ~/Library/Keychains/login.keychain-db ./certificates/sunspot.pem
+  security add-trusted-cert -p ssl -k ~/Library/Keychains/login.keychain-db ./certs/sunspot.pem
+  echo "Done!"
+elif [ "$(uname)" == "Linux" ] && [ -d "/usr/local/share/ca-certificates/" ]; then
+  echo "Copying \`Sunspot\` Certificate Authority to \`/usr/local/share/ca-certificates/sunspot.crt\` and updating trusted certificates..."
+  sudo cp ./certs/sunspot.pem /usr/local/share/ca-certificates/sunspot.crt
+  sudo update-ca-certificates
   echo "Done!"
 else
-  echo "Please add the certificate to your trusted certificates and enable it for SSL"
+  echo "Please add \`./certs/sunspot.pem\` to your trusted certificates."
 fi
